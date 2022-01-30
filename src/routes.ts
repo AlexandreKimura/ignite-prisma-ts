9b48ee39d28e0602757ab0a2438bc4a9d3ebff32
Alexandre Fukano <br-amrcld-alexandrefukano@kpmgamr.onmicrosoft.com>
2022-01-30 18:46:05 -0300

    Find all deliveries by client

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,8 @@ import { ensureAuthenticateClient } from "./middlewares/ensureAuthenticateClient
 import { ensureAuthenticateDeliveryman } from "./middlewares/ensureAuthenticateDeliveryman"
 import { AuthenticateClientController } from "./modules/accounts/authenticateClient/AuthenticateClientController"
 import { AuthenticateDeliverymanController } from "./modules/accounts/authenticateDeliveryman/AuthenticateDeliverymanController"
-import { CreateClientController } from "./modules/clients/useCases/CreateClient/CreateClientController"
+import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController"
+import { FindAllDeliveriesController } from "./modules/clients/useCases/findDeliveries/findAllDeliveriesController"
 import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController"
 import { FindAllAvailableDeliveriesController } from "./modules/deliveries/useCases/findAllAvailableDeliveries/findAllAvailableDeliveriesController"
 import { UpdateDeliverymanController } from "./modules/deliveries/useCases/updateDeliveryman/UpdateDeliverymanController"
@@ -19,11 +20,13 @@ const createDeliverymanController = new CreateDeliverymanController()
 const createDeliveryController = new CreateDeliveryController()
 const findAllWithoutEndDateController = new FindAllAvailableDeliveriesController()
 const updateDeliverymanController = new UpdateDeliverymanController()
+const findAllDeliveriesController = new FindAllDeliveriesController()
 
 routes.post("/client/authenticate/", authenticateClientController.handle)
 routes.post("/deliveryman/authenticate/", authenticateDeliverymanController.handle)
 
 routes.post("/client/", createClientController.handle)
+routes.get("/client/deliveries", ensureAuthenticateClient, findAllDeliveriesController.handle)
 routes.post("/deliveryman/", createDeliverymanController.handle)
 
 routes.post("/delivery/", ensureAuthenticateClient, createDeliveryController.handle)
